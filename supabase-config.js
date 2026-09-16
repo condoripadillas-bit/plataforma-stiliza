34e2cfc4342d98b463a46d323313e6d4a5804407
condoripadillas-bit <condoripadillas@gmail.com>
2026-09-16 17:00:07 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.STILIZA_SUPABASE = {
   url: 'https://TU-PROYECTO.supabase.co',
-  anonKey: 'TU-ANON-KEY'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkZXpveGhnZWN2ZnhwdWRzcWxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNTMwNTgsImV4cCI6MjEwNDcyOTA1OH0.nRBHXgH-pKO0qu9pcFIIddKjql696puiqqRu_CUcdgU'
 };
